8ddcf2cef2e9b2c8681adcd5247b1b587b893fb7
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-05-12 21:07:33 +0900

    add provideApiService

--- app/src/main/java/io/github/sassy/githubrepolist/di/AppModule.kt
+++ app/src/main/java/io/github/sassy/githubrepolist/di/AppModule.kt
@@ -2,12 +2,27 @@ package io.github.sassy.githubrepolist.di
 
 import dagger.Module
 import dagger.Provides
+import io.github.sassy.githubrepolist.api.ApiService
 import io.github.sassy.githubrepolist.repository.RepoRepository
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module(includes = [ViewModelModule::class])
 class AppModule {
     @Singleton
     @Provides
-    fun providerRepoRepository() = RepoRepository()
+    fun provideApiService() : ApiService {
+        return Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun providerRepoRepository(apiService: ApiService) = RepoRepository(apiService)
 }
