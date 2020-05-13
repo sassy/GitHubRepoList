931e6700415a55a2df6c55bb27713d9bbea17da8
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-05-13 14:31:26 +0900

    add navigation.

--- app/src/main/java/io/github/sassy/githubrepolist/MainActivity.kt
+++ app/src/main/java/io/github/sassy/githubrepolist/MainActivity.kt
@@ -2,12 +2,11 @@ package io.github.sassy.githubrepolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
-import io.github.sassy.githubrepolist.ui.RepoFragment
+import io.github.sassy.githubrepolist.ui.repo.RepoFragment
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(),
--- app/src/main/java/io/github/sassy/githubrepolist/di/FragmentBuildersModule.kt
+++ app/src/main/java/io/github/sassy/githubrepolist/di/FragmentBuildersModule.kt
@@ -2,7 +2,7 @@ package io.github.sassy.githubrepolist.di
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import io.github.sassy.githubrepolist.ui.RepoFragment
+import io.github.sassy.githubrepolist.ui.repo.RepoFragment
 
 @Suppress("unused")
 @Module
